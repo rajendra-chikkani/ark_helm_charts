4a2ca97f0d13c6452a9b50f18a1088109423318a
rajendra-chikkani <132587062+rajendra-chikkani@users.noreply.github.com>
2023-07-03 20:16:30 +0530

    Updated helm-chart-develop.ps1
    Branch specific helm deployment is working now, tested on main and feature/memspec branches

--- helm-chart-develop.ps1
+++ helm-chart-develop.ps1
@@ -1,38 +1,98 @@
-# Set the Git repository URL
-$gitRepository = "https://github.com/ArkCase/ark_helm_charts"
+function Deploy-HelmChart {
+    param (
+        [Parameter(Mandatory=$true)]
+        [ValidateNotNullOrEmpty()]
+        [string]$BranchName
+    )
 
-# Clone the Git repository to a specific directory using the Git command
-$cloneDirectory = "./mychart"
-git clone $gitRepository $cloneDirectory
+    # Set the Git repository URL
+    $gitRepository = "https://github.com/rajendra-chikkani/ark_helm_charts"
 
-# Get the current Git branch
-$gitBranch = git rev-parse --abbrev-ref HEAD
+    # Clone the Git repository to a specific directory using the Git command
+    $cloneDirectory = "arkcase/app"
+    git clone $gitRepository $cloneDirectory
+    Set-Location $cloneDirectory
 
-# Determine the environment and values file based on the Git branch
-if ($gitBranch -eq "develop") {
-    $environment = "dev"
-    $valuesFile = "values-dev.yaml"
-} elseif ($gitBranch -eq "main") {
-    $environment = "prod"
-    $valuesFile = "values-prod.yaml"
-} else {
-    Write-Host "Invalid branch."
+    # Fetch branch names from the remote repository
+    git fetch
+
+    # Get the current branch name
+    $gitBranch = git rev-parse --abbrev-ref HEAD
+
+    if ($gitBranch -eq "main") {
+        $environment = "prod"
+        $valuesFile = "values-$environment.yaml"
+    } elseif ($gitBranch -eq "feature/memspec") {
+        $environment = "dev"
+        $valuesFile = "values-dev.yaml"
+    } else {
+        $environment = $BranchName.ToLower()
+        $valuesFile = "values-$environment.yaml"
+    }
+
+    # Check if the specified branch exists in the repository
+    $remoteBranch = "origin/$BranchName"
+
+    if (git branch --list --remote $remoteBranch) {
+        # Switch to the specified Git branch
+        git checkout $remoteBranch
+
+        # Determine the environment and values file based on the Git branch
+        if ($BranchName -eq "feature/memspec") {
+            $environment = "dev"
+            $valuesFile = "values-dev.yaml"
+        } elseif ($BranchName -eq "main") {
+            $environment = "prod"
+            $valuesFile = "values-$environment.yaml"
+        } else {
+            $environment = $BranchName.ToLower()
+            $valuesFile = "values-$environment.yaml"
+        }
+
+        # Set the chart path
+        $chartPath = $PWD.Path
+
+        # Verify if the values file exists in the specified location
+        $valuesFilePath = Join-Path -Path $chartPath -ChildPath $valuesFile
+        if (-not (Test-Path -Path $valuesFilePath)) {
+            Write-Host "Values file '$valuesFile' not found in the chart directory."
+            exit 1
+        }
+    } else {
+        Write-Host "Branch '$BranchName' not found in the remote repository."
+        exit 1
+    }
+
+    # Display the determined environment
+	Write-Host ""
+    Write-Host "Environment: $environment"
+	Write-Host ""
+	Write-Host "Current directory: $chartPath"
+    Write-Host ""
+    $releaseName = "arkcase"
+	
+	Set-Location src/app
+
+    # Run Helm command to deploy the chart
+    helm install $releaseName . --values $valuesFilePath
+	Write-Host ""
+}
+
+# Parse the script arguments
+$BranchName = $null
+
+for ($i = 0; $i -lt $args.Length; $i++) {
+    if ($args[$i] -eq "-helm-branch" -and $i + 1 -lt $args.Length) {
+        $BranchName = $args[$i + 1]
+        break
+    }
+}
+
+# Check if a branch name was provided
+if (-not $BranchName) {
+    Write-Host "Please provide a branch name using '-helm-branch' argument."
     exit 1
 }
 
-
-# Prompt the user if an environment was not determined
-if ($environment -eq $null) {
-    $environment = Read-Host "Enter the environment (dev/main):"
-    $valuesFile = "values-$environment.yaml"
-}
-
-# Display the determined environment
-Write-Host "Environment: $environment"
-
-# Set the chart path
-$chartPath = $cloneDirectory
-$releaseName = "mychart-release"
-
-# Run Helm command to deploy the chart
-helm install --name $releaseName --values $valuesFile $chartPath
+# Call the function and pass the Git branch as an argument
+Deploy-HelmChart -BranchName $BranchName
